1b7d11f946baacb59f047272b6da3383e3167d17
rabbi0197273 <myweb063@gmail.com>
2025-06-15 16:15:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-function getparam(a, e) {
-  return e || (e = window.location.href), new URL(e).searchParams.get(a)
-}
-const s = (a) => document.getElementById(a)
 
-function getIdFromUrl(url) {
-  return url.match(/[-\w]{25,}/)
-}
-const apikey = "AIzaSyD739-eb6NzS_KbVJq1K8ZAxnrMfkIqPyw"
 
-const get = () => {
-  getbase()
+function getparam(a,e){return e||(e=window.location.href),new URL(e).searchParams.get(a)}
+let s=a=>document.getElementById(a);
+
+
+function getIdFromUrl(url) { return url.match(/[-\w]{25,}/); }
+let apikey = 'AIzaSyD739-eb6NzS_KbVJq1K8ZAxnrMfkIqPyw';
+
+let get=()=>{
+  getbase();
 }
-const getbase = () => {
-  const ply = []
+let getbase=()=>{
+    let ply = [];
   /*
     ply.videourl = `https://www.googleapis.com/drive/v3/files/${getIdFromUrl(s('videourl').value) }?alt=media&key=${apikey}`;
 
@@ -25,115 +24,91 @@ const getbase = () => {
     poster = 'https://lh3.googleusercontent.com/d/'+getIdFromUrl(s('videourl').value);
   }
     ply.posterurl = poster ; */
-
-  ply.id = getIdFromUrl(s("videourl").value)
-
-  // ply.videotitle = s('videotitle').value;
-  console.log(ply)
-
-  var arr = JSON.stringify(Object.assign({}, ply))
-  console.log(btoa(arr))
-  window.base = btoa(arr)
-  iframe()
-  return btoa(arr)
+  
+    ply.id =  getIdFromUrl(s('videourl').value);
+      
+   // ply.videotitle = s('videotitle').value;
+    console.log(ply);
+  
+    var arr = JSON.stringify(Object.assign({}, ply))
+    console.log(btoa(arr));
+    window.base =btoa(arr);
+    iframe();
+    return btoa(arr);
 }
+
 
 //Player Openers (diff. func. coding intentionally)
-const openartplayer = () => {
-  window.open("https://rabbi0197273.github.io/Rabbi123/artplayer.html?id=" + base)
-}
-const openplyr = () => {
-  window.open("https://rabbi0197273.github.io/Rabbi123/plyr.html?id=" + base)
-}
-const openfluid = () => {
-  window.open("https://rabbi0197273.github.io/Rabbi123/fluid.html?id=" + base)
-}
-const openafterglow = () => {
-  window.open("https://rabbi0197273.github.io/Rabbi123/afterglow.html?id=" + base)
-}
-const openmediaelements = () => {
-  window.open("https://rabbi0197273.github.io/Rabbi123/mediaelements.html?id=" + base)
-}
-const openvlitejs = () => {
-  window.open("https://rabbi0197273.github.io/Rabbi123/vlitejs.html?id=" + base)
-}
+let opensp=()=> { window.open('https://rabbi0197273.github.io/Rabbi123/sopplayer.html?id='+base)}
+let openplyr=()=> { window.open('https://rabbi0197273.github.io/Rabbi123/plyr.html?id='+base)}
+let openfluid=()=> { window.open('https://rabbi0197273.github.io/Rabbi123/fluid.html?id='+base)}
+let openafterglow=()=> { window.open('https://rabbi0197273.github.io/Rabbi123/afterglow.html?id='+base)}
+let openmediaelements=()=> { window.open('https://rabbi0197273.github.io/Rabbi123/mediaelements.html?id='+base)}
+let openvlitejs=()=> { window.open('https://rabbi0197273.github.io/Rabbi123/vlitejs.html?id='+base)}
 
-const pmsg = "Copy Embed Code"
+
+let pmsg = 'Copy Embed Code' ;
 //Player Embedders
-const embedartplayer = () => {
-  prompt(
-    pmsg,
-    `<iframe width="560" height="315" 
+let embedsp=()=> {
+  prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
-src="https://rabbi0197273.github.io/Rabbi123/artplayer.html?id=${base}" 
+src="https://rabbi0197273.github.io/Rabbi123/sopplayer.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
-</iframe>`,
-  )
+</iframe>`)
 }
-const embedplyr = () => {
-  prompt(
-    pmsg,
-    `<iframe width="560" height="315" 
+let embedplyr=()=> {
+  prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
 src="https://rabbi0197273.github.io/Rabbi123/plyr.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
-</iframe>`,
-  )
+</iframe>`)
 }
-const embedfluid = () => {
-  prompt(
-    pmsg,
-    `<iframe width="560" height="315" 
+let embedfluid=()=> {
+  prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
 src="https://rabbi0197273.github.io/Rabbi123/fluid.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
-</iframe>`,
-  )
+</iframe>`)
 }
-const embedafterglow = () => {
-  prompt(
-    pmsg,
-    `<iframe width="560" height="315" 
+let embedafterglow=()=> {
+  prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
 src="https://rabbi0197273.github.io/Rabbi123/afterglow.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
-</iframe>`,
-  )
+</iframe>`)
 }
-const embedmediaelements = () => {
-  prompt(
-    pmsg,
-    `<iframe width="560" height="315" 
+let embedmediaelements=()=> {
+  prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
 src="https://rabbi0197273.github.io/Rabbi123/mediaelements.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
-</iframe>`,
-  )
+</iframe>`)
 }
-const embedvlitejs = () => {
-  prompt(
-    pmsg,
-    `<iframe width="560" height="315" 
+let embedvlitejs=()=> {
+  prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
 src="https://rabbi0197273.github.io/Rabbi123/vlitejs.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
-</iframe>`,
-  )
+</iframe>`);
 }
 
-const iframe = () => {
-  s("afterglow").src = "https://rabbi0197273.github.io/Rabbi123/afterglow.html?id=" + base
-  s("fluid").src = "https://rabbi0197273.github.io/Rabbi123/fluid.html?id=" + base
-  s("plyr").src = "https://rabbi0197273.github.io/Rabbi123/plyr.html?id=" + base
 
-  s("vlitejs").src = "https://rabbi0197273.github.io/Rabbi123/vlitejs.html?id=" + base
-  s("mediaelements").src = "https://rabbi0197273.github.io/Rabbi123/mediaelements.html?id=" + base
 
-  s("artplayer").src = "https://rabbi0197273.github.io/Rabbi123/artplayer.html?id=" + base
+let iframe=()=> {
+  s('afterglow').src= 'https://rabbi0197273.github.io/Rabbi123/afterglow.html?id='+base;
+  s('fluid').src='https://rabbi0197273.github.io/Rabbi123/fluid.html?id='+base;
+  s('plyr').src='https://rabbi0197273.github.io/Rabbi123/plyr.html?id='+base;
+
+  s('vlitejs').src='https://rabbi0197273.github.io/Rabbi123/vlitejs.html?id='+base;
+  s('mediaelements').src='https://rabbi0197273.github.io/Rabbi123/mediaelements.html?id='+base;
+  
+    s('sopplayer').src='https://rabbi0197273.github.io/Rabbi123/sopplayer.html?id='+base;
+  
 }
+
